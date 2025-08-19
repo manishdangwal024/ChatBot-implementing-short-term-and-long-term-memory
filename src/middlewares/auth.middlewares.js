953c877df06c6f1e1724fd018a438d871e4c8ba6
manishdangwal024 <manishdangwal024@gmail.com>
2025-08-19 10:21:29 +0530

    second commit

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -2,11 +2,12 @@ const userModel=require("../models/user.model")
 const jwt=require("jsonwebtoken")
 
 async function authMiddleware(req,res,next) {
-    const token =req.cookies;
+    const {token} =req.cookies;
     if(!token){
        return res.status(401).json({message:"Unautharized"})
     }
     try {
+        
         const decoded= jwt.verify(token,process.env.JWT_SECRET)
         const user=await userModel.findById(decoded.id).select("-password");
         req.user=user
